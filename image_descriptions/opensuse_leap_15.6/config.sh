5600a37a3e0c4b931b5f260b46c008cc1c778a95
Bas van Kampen <bas.vankampen@suse.com>
2026-07-31 15:46:18 +0200

    Fix here-document EOF indentation in config.sh templates

--- image_descriptions/opensuse_leap_15.6/config.sh
+++ image_descriptions/opensuse_leap_15.6/config.sh
@@ -168,15 +168,15 @@ if [[ "$kiwi_profiles" == *"RaspberryPi"* ]]; then
         echo 'add_drivers+=" bcm2835_dma dwc2 "' > /etc/dracut.conf.d/raspberrypi_modules.conf
 
         # Work around network issues
-        cat > /etc/modprobe.d/50-rpi3.conf <<-EOF
+        cat > /etc/modprobe.d/50-rpi3.conf <<'EOF'
                 # Prevent too many page allocations (bsc#1012449)
                 options smsc95xx turbo_mode=N
-        EOF
+EOF
 
-        cat > /usr/lib/sysctl.d/50-rpi3.conf <<-EOF
+        cat > /usr/lib/sysctl.d/50-rpi3.conf <<'EOF'
                 # Avoid running out of DMA pages for smsc95xx (bsc#1012449)
                 vm.min_free_kbytes = 2048
-        EOF
+EOF
 fi
 
 #=========================================================
